7abbe6cfe7b2da794d2dd00776325fa758c6dde8
mthsps <dmatheusps@gmail.com>
2022-03-19 18:24:40 -0300

    Assignment 4: Redux, Client-Server Communication and Fetch

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label } from 'reactstrap';
-import { Control, Form, Errors, actions } from 'react-redux-form';
+import { Control, Form, Errors} from 'react-redux-form';
+import { postFeedback } from '../redux/ActionCreators';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -19,9 +20,11 @@ class Contact extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        console.log('Thank you for your feedback! ' + JSON.stringify(values));
+        alert('Thank you for your feedback! ' + JSON.stringify(values));
         this.props.resetFeedbackForm();
+        this.props.postFeedback(values.firstname, values.lastname, 
+            values.telnum, values.email, values.agree, values.contactType, values.message);
         // event.preventDefault();
     }
 
@@ -69,7 +72,7 @@ class Contact extends Component {
                       <h3>Send us your Feedback</h3>
                    </div>
                     <div className="col-12 col-md-9">
-                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)} postFeedback={this.props.postFeedback}>
                     <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -5,6 +5,8 @@ import { baseUrl } from '../shared/baseUrl';
 import { FadeTransform } from 'react-animation-components';
 
 function RenderCard({item, isLoading, errMess}) {
+
+    console.log("load: " + isLoading + " " + errMess)
     
     if (isLoading) {
         return(
@@ -55,7 +57,11 @@ function Home(props) {
                 />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                    <RenderCard 
+                        item={props.leader}
+                        isLoading={props.leadersLoading}
+                        errMess={props.leadersErrMess}
+                    />
                 </div>
             </div>
         </div>
