b016785bc114c2e0a9d733076cac717f90ae96f1
mthsps <dmatheusps@gmail.com>
2022-03-19 11:47:40 -0300

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -13,6 +14,11 @@ const minLength = (len) => (val) => val && (val.length >= len);
 function RenderDish({ dish }) {
   return (
     <div>
+    <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
       <Card width="18rem">
       <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
@@ -20,6 +26,7 @@ function RenderDish({ dish }) {
           <CardText>{dish.description}</CardText>
         </CardBody>
       </Card>
+      </FadeTransform>
     </div>
   );
 }
@@ -30,18 +37,22 @@ function RenderComments({ comments, postComment, dishId }) {
       <div>
         <h4>Comments</h4>
         <ul className="list-unstyled">
+        <Stagger in>
           {comments.map((comment) => {
             return (
               <div>
+              <Fade in>
                 <li key={comment.id}>
                   <ul className="mt-3">{comment.comment}</ul>
                   <ul className="mt-3">
                     -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                   </ul>
                 </li>
+              </Fade>
               </div>
             );
           })}
+        </Stagger>
         </ul>
         <CommentForm dishId={dishId} postComment={postComment} />
       </div>
